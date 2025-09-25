7a9a7e14e3a5ca2f2f7de7e7adc25d314c0f8028
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-25 13:40:52 +0300

    pää program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿namespace P_iv_n_astronominen_kuva_NASA_;
+﻿using System.Net.Http.Headers;
+using System.Globalization;
+
+
+namespace P_iv_n_astronominen_kuva_NASA_;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Minkä ajan kuvan haluat ladata ?");
+        while (true)
+        {
+            Console.WriteLine("Anna päivämäärä muodossa yyyy-MM-dd (esim. 2025-09-25)");
+            string? input = Console.ReadLine();
+
+            if (DateTime.TryParseExact(input, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime päivämäärä))
+            {
+                Console.WriteLine($"Annoit päivämäärän: {päivämäärä:yyyy.mm.dd}");
+                break; // poistutaan loopista kun saatiin kelvollinen arvo
+            }
+            else
+            {
+                Console.WriteLine("Virheellinen syöte! Kokeile uudestaan.");
+            }
+        }
     }
 }
